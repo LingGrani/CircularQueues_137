f584e3969e223c87c0d99d3837ac6b33fa26f978
LingGrani <luthfiangger2510@gmail.com>
2023-06-22 15:06:02 +0700

    ketika rear ada diposisi terakhir

--- CircularQueues_137/CircularQueues_137.cpp
+++ CircularQueues_137/CircularQueues_137.cpp
@@ -20,8 +20,15 @@ public:
 			return;
 		}
 		//cek antrian kosong
-		if (FRONT == -1 && REAR == -1) {
-
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == Max - 1)
+				REAR = 0;
+			
 		}
 	}
 };
